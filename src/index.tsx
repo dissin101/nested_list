c304889b8e950bb6ca980d5c04523ff353b7e2bd
Kirill Bushmanov <dissin101@gmail.com>
2024-10-05 19:32:02 +0500

    Project init

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Container from "./components/Container";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(<Container />);
